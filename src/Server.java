882387da0679a2b222680efb6b442f697bdbf8a6
Trevor Farrelly <farrellytrevor@gmail.com>
2019-02-14 14:07:32 -0500

    Add code skeleton

---
+++ src/Server.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+
+    // server parameters
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+}
+
+public static void main(String args[]) {
+    System.out.println("tes");
+}
