5ae9a020a5007a54e259a0516e25532fc964fb48
Corey Boornazian <coreyb220@gmail.com>
2019-03-17 17:05:26 -0400

    Add DbWrapper class and a extract a few basic functions

--- src/DbWrapper.java
+++ src/DbWrapper.java
@@ -1,57 +1,19 @@
 package src;
 
-/*
-
-   Derby - Class SimpleApp
-
-   Licensed to the Apache Software Foundation (ASF) under one or more
-   contributor license agreements.  See the NOTICE file distributed with
-   this work for additional information regarding copyright ownership.
-   The ASF licenses this file to You under the Apache License, Version 2.0
-   (the "License"); you may not use this file except in compliance with
-   the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
- */
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import java.util.ArrayList;
 import java.util.Properties;
 
 
 /**
- * <p>
- * This sample program is a minimal Java application showing JDBC access to a
- * Derby database.</p>
- * <p>
- * Instructions for how to run this program are
- * given in <A HREF=example.html>example.html</A>, by default located in the
- * same directory as this source file ($DERBY_HOME/demo/programs/simple/).</p>
- * <p>
- * Derby applications can run against Derby running in an embedded
- * or a client/server framework.</p>
- * <p>
- * When Derby runs in an embedded framework, the JDBC application and Derby
- * run in the same Java Virtual Machine (JVM). The application
- * starts up the Derby engine.</p>
- * <p>
- * When Derby runs in a client/server framework, the application runs in a
- * different JVM from Derby. The connectivity framework (in this case the Derby
- * Network Server) provides network connections. The client driver is loaded
- * automatically.</p>
+ * DbWrapper abstracts all necessary DB functionality that we need for Wysper
+ * @TODO: Should store a map from userid to lastRead message and update when we get a pull
+ * so we know how many messages to return
+ *
+ * @TODO: Need to write a function to return the n most recent messages from the messages table
+ *
+ * @TODO
  */
 public class DbWrapper
 {
@@ -63,41 +25,20 @@ public class DbWrapper
     private Connection conn;
 
     /**
-     * <p>
-     * Starts the demo by creating a new instance of this class and running
-     * the <code>go()</code> method.</p>
-     * <p>
-     * When you run this application, you may give one of the following
-     * arguments:
-     *  <ul>
-     <li><code>embedded</code> - default, if none specified. Will use
-     *        Derby's embedded driver. This driver is included in the derby.jar
-     *        file.</li>
-     *    <li><code>derbyclient</code> - will use the Derby client driver to
-     *        access the Derby Network Server. This driver is included in the
-     *        derbyclient.jar file.</li>
-     *  </ul>
-     * <p>
-     * When you are using a client/server framework, the network server must
-     * already be running when trying to obtain client connections to Derby.
-     * This demo program will will try to connect to a network server on this
-     * host (the localhost), see the <code>protocol</code> instance variable.
-     * </p>
-     * <p>
-     * When running this demo, you must include the correct driver in the
-     * classpath of the JVM. See <a href="example.html">example.html</a> for
-     * details.
-     * </p>
-     * @param args This program accepts one optional argument specifying which
-     *        connection framework (JDBC driver) to use (see above). The default
-     *        is to use the embedded JDBC driver.
+     * The main function demonstrates some basic functionality of our dbwrapper class
+     * You should call the constructor in the client function to connect to the
+     * embedded database.
+     * @param args
      */
     public static void main(String[] args)
     {
         //new DbWrapper().go();
         DbWrapper db = new DbWrapper();
         db.createUsersTable();
+        db.createMessagesTable();
         db.insertUser("Corey");
+        db.insertMessage();
+        db.dropMessagesTable();
         db.dropUsersTable();
         System.out.println("SimpleApp finished");
     }
@@ -142,7 +83,7 @@ public class DbWrapper
             this.conn = DriverManager.getConnection(protocol + dbname
                     + ";create=true");
 
-            System.out.println("Connected to and created database " + dbname);
+            System.out.println("Created database " + dbname + " and connected");
 
             // We want to control transactions manually. Autocommit is on by
             // default in JDBC.
@@ -156,7 +97,7 @@ public class DbWrapper
     }
 
     /**
-     * Makes the users table on our connection object
+     * Initializes the users table according to our Sprint2/DB Schema doc
      */
     private void createUsersTable() {
 
@@ -180,11 +121,35 @@ public class DbWrapper
 
     }
 
-
+    /**
+     * Initializes the messages table according to our Sprint2/DB Schema document
+     */
     private void createMessagesTable() {
+        try {
+            Statement s;
+            s = this.conn.createStatement();
+            statements.add(s);
 
+            // TODO: Should check here if the tables are already created
+            // TODO: how big should message body be?
+            s.execute("create table messages(" +
+                    "mid int NOT NULL GENERATED ALWAYS AS IDENTITY, " +
+                    "userid int, " +
+                    "time timestamp, " +
+                    "text varchar(255), " +
+                    "PRIMARY KEY(mid)" +
+                    ")");
+            System.out.println("Created table messages");
+        }
+        catch (SQLException sqle) {
+            printSQLException(sqle);
+        }
     }
 
+    /**
+     * This function adds a new user to the database
+     * @param username the user to add to the db
+     */
     private void insertUser(String username) {
         PreparedStatement psInsert;
         try {
@@ -195,15 +160,39 @@ public class DbWrapper
             psInsert.setString(1, username);     // Set username to corey
             psInsert.setInt(2, 0);              // Set lastRead to 0
             psInsert.executeUpdate();
-            System.out.println("Inserted" + username + "into Users");
+            System.out.println("Inserted " + username + " into users");
         }
         catch (SQLException sqle) {
             printSQLException(sqle);
         }
     }
 
-    private void insertMessagesRow() {
+    /**
+     * This function adds a message to our database
+     * TODO: Add message to parameters and extract necessary info
+     * In wysper-server, we have a Message object that helps pass messages around,
+     * but I'm not sure how you want to implement that Trevor so I'll leave it like this
+     */
+    private void insertMessage() {
+        int userid = 20;
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        String text = "Whats up";
 
+        PreparedStatement psInsert;
+        try {
+            psInsert = conn.prepareStatement(
+                    "insert into messages (userid, time, text) values (?, ?, ?)");
+            statements.add(psInsert);
+
+            psInsert.setInt(1, userid);     // Set userid
+            psInsert.setTimestamp(2, time); // Set timestamp
+            psInsert.setString(3, text);    // Set message body
+            psInsert.executeUpdate();
+            System.out.println("Inserted \"" + text + "\" into messages");
+        }
+        catch (SQLException sqle) {
+            printSQLException(sqle);
+        }
     }
 
     private void updateUsersRow() {
@@ -214,6 +203,9 @@ public class DbWrapper
 
     }
 
+    /**
+     * Drops the user table from our database
+     */
     private void dropUsersTable() {
         try {
             Statement s = conn.createStatement();
@@ -226,6 +218,30 @@ public class DbWrapper
             printSQLException(sqle);
         }
     }
+
+    /**
+     * Drops the messages table from our database
+     */
+    private void dropMessagesTable() {
+        try {
+            Statement s = conn.createStatement();
+            statements.add(s);
+            // delete the table
+            s.execute("drop table messages");
+            System.out.println("Dropped table messages");
+        }
+        catch (SQLException sqle) {
+            printSQLException(sqle);
+        }
+    }
+
+    /**
+     * This function was provided with the derby installation as a sample app
+     * I extracted the functionality into separate functions, but I left this
+     * in case we need to refer back to it in the future.
+     * TODO: remove it?
+     */
+
 
     /**
      * <p>
